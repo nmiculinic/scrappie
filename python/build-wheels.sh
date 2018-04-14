cb1f35175258c8d5e2b5edbabc9db708ab539962
Chris Wright <chris.wright@nanoporetech.com>
2018-04-14 17:35:27 +0100

    Build manylinux with openblas

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 set -e -x
+
+# this is used in build.py
 export MANYLINUX=1
 
-# Install a system package required by our library
-yum install -y atlas-devel
-ln -s /usr/lib64/atlas/libcblas.so /usr/lib64/libblas.so
+BUILD_PREFIX="/usr/local"
+OPENBLAS_VERS="0.2.18"
+OPENBLAS_TAR=/io/python/openblas_${OPENBLAS_VERS}.tgz
+
+function build_openblas {
+    # this takes a long time so record success in openblas-built
+    if [ -e "openblas-built" ]; then return; fi
+    if [ -d "OpenBLAS" ]; then
+        (cd OpenBLAS && git clean -fxd && git reset --hard)
+    else
+        git clone https://github.com/xianyi/OpenBLAS
+    fi
+    (cd OpenBLAS \
+        && git checkout "v${OPENBLAS_VERSION}" \
+        && make DYNAMIC_ARCH=1 USE_OPENMP=0 NUM_THREADS=64 TARGET=NEHALEM > /dev/null)
+    touch openblas-built
+}
+
+function install_openblas {
+    (cd OpenBLAS && make PREFIX=$BUILD_PREFIX install)
+    tar zcf ${OPENBLAS_TAR} /usr/local/lib /usr/local/include
+}
+
+
+cd /io
+# if there's a openblas tar lying around use it, else download,
+# build, and install (making the tar along the way).
+if [ -e ${OPENBLAS_TAR} ]; then
+    tar xzf ${OPENBLAS_TAR} -C /
+else
+    build_openblas
+    install_openblas
+fi
+
 
 cd /io/python
+rm -rf wheelhouse
 
-# Compile wheels
+# compile wheels
 for minor in 4 5 6; do
     PYBIN="/opt/python/cp3${minor}-cp3${minor}m/bin"
     "${PYBIN}/pip" wheel . -w wheelhouse/
 done
 
-
-# Bundle external shared libraries into the wheels
 for whl in wheelhouse/scrappy*.whl; do
     auditwheel repair "$whl" -w wheelhouse/
+    rm "$whl"
 done
 
-
-# Install packages and "test"
+# install and "test"
 for minor in 4 5 6; do
     PYBIN="/opt/python/cp3${minor}-cp3${minor}m/bin"
     "${PYBIN}/pip" install scrappy --no-index -f wheelhouse
